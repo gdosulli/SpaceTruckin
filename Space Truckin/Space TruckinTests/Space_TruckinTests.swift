d0c71b7e78ae824c1c5ef678aab0a825de2bee1e
Ethan Nerney <enerney@uvm.edu>
2020-10-05 15:41:52 -0400

    created xcode project

---
+++ Space Truckin/Space TruckinTests/Space_TruckinTests.swift
@@ -0,0 +1,34 @@
+//
+//  Space_TruckinTests.swift
+//  Space TruckinTests
+//
+//  Created by Ethan Nerney on 10/5/20.
+//  Copyright © 2020 SpaceTruckin. All rights reserved.
+//
+
+import XCTest
+@testable import Space_Truckin
+
+class Space_TruckinTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
