34bf77b461b824d60f5c8329fff539de3ae89382
Ethan Nerney <enerney@uvm.edu>
2020-10-05 16:52:02 -0400

    Created Xcode Project made some emitters and added the truck placeholder images to game Worked on truck scene and discovered why the sk scene and .swift files weren't connecting

---
+++ Space Truckin/Space Truckin/Truck.swift
@@ -0,0 +1,18 @@
+//
+//  Truck.swift
+//  Space Truckin
+//
+//  Created by Ethan Nerney on 10/5/20.
+//  Copyright © 2020 SpaceTruckin. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+class Truck {
+//    let head: SKSpriteNode!
+//    var trailers: [SKSpriteNode]
+    
+    
+}
---
+++ Space Truckin/Space Truckin/TruckScene.swift
@@ -0,0 +1,21 @@
+//
+//  TruckScene.swift
+//  Space Truckin
+//
+//  Created by Ethan Nerney on 10/5/20.
+//  Copyright © 2020 SpaceTruckin. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+
+class TruckScene: SKScene {
+    var head: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        head = SKSpriteNode(imageNamed: "space_truck_cab")
+        head.position = CGPoint(x:0,y:0)
+        head.zPosition = 1
+        self.addChild(head)
+        print("Did move")
+    }
+}
