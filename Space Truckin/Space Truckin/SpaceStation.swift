1788e10822a8a9e942f65ba752c67283dab197b1
Ethan Nerney <enerney@uvm.edu>
2020-12-02 23:56:55 -0500

    space station

--- Space Truckin/Space Truckin/SpaceStation.swift
+++ Space Truckin/Space Truckin/SpaceStation.swift
@@ -68,23 +68,12 @@ class SpaceStation: SpaceObject {
     
     func dock(_ piece: TruckPiece){
         sprite.isPaused = true
-//        let scene = (sprite.parent as! AreaScene)
-//        let cam = scene.cam
-//        stationMenu.frameSize = CGSize(width: scene.frame.width * scene.camScale, height: scene.frame.height * scene.camScale)
-//        stationMenu.reinit()
-//        stationMenu.background.position = cam.position
-//        sprite.parent?.addChild(stationMenu.background)
-//    
-//        
-//        
-        stationMenu.truckHead = piece
-        stationMenu.show()
         //set head
         //show screen
         print("DOCKED")
     }
     
-    func undock(){
+    func undock(_ piece: TruckPiece){
         sprite.isPaused = false
         //remove head reference?
         //hide screen
