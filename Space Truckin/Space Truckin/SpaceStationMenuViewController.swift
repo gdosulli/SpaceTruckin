6c76fdd80dd866f3e1a20b80c52cb72576c25960
Ethan Nerney <enerney@uvm.edu>
2020-12-09 23:38:03 -0500

    truck station stuff being added also working on a potential storyboard solution to the space station menu

---
+++ Space Truckin/Space Truckin/SpaceStationMenuViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SpaceStationMenuViewController.swift
+//  Space Truckin
+//
+//  Created by Ethan Nerney on 12/9/20.
+//  Copyright © 2020 SpaceTruckin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SpaceStationMenuView: UIViewController {
+    
+    @IBOutlet var topView: UICollectionView!
+    @IBOutlet var middleView: UICollectionView!
+    @IBOutlet var bottomView: UICollectionView!
+    
+    
+    // what the space station's selling, and at what price
+    var spaceStationGoods = [(Item, Int)]()
+    // "" "" buying "" ""
+    var spaceStationBuying = [(Item,Int)]()
+    
+    var playerGarage = [TruckPiece]()
+    
+    var playerTruckHead: TruckPiece!
+    
+}
